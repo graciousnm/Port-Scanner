7e5ba8ae465952e2e0aa2048e585901659bbce6a
Bernard Adutwum <159218910+graciousnm@users.noreply.github.com>
2024-02-09 14:41:43 -0400

    Update port_scan.py

--- port_scan.py
+++ port_scan.py
@@ -13,9 +13,9 @@ banner = Fore.RED+ r"""
                     |  __/ (_) | |  | |_      ___) | (_| (_| | | | | | | |  __/ |   
                     |_|   \___/|_|   \__|    |____/ \___\__,_|_| |_|_| |_|\___|_| 
 
-                    Version:1.0 - By: @gracious
-                    https://github.com/graciousnm
-                    Use at your own risk
+                                      Version:1.0 - By: @gracious
+                                      https://github.com/graciousnm
+                                      Use at your own risk
 
                     ######################****************#######################
 """
